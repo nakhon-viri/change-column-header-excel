889022854fb3d61de785a776aa01728ec82c78c0
DESKTOP-S609Q5H\NNN <nakhon.viri@bumail.net>
2024-01-13 17:52:49 +0700

    '1234'

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App_xlsx.tsx'
 import './index.css'
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import { PrimeReactProvider } from 'primereact/api';
 ReactDOM.createRoot(document.getElementById('root')!).render(
-
   <PrimeReactProvider>
     <App />
   </PrimeReactProvider>
